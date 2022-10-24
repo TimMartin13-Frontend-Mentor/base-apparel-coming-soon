69909920ae2663218059fd83d909b391b002b25e
Tim Martin <timmartin13@gmail.com>
2022-10-24 15:10:54 -0500

    Added Logo, image, headline and copy for mobile

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,13 +6,41 @@ function Card() {
     <main className={card.wrapper}>
       <header>
         <img
-          className={card.hero_image}
-          alt='young lady'
-          // src={process.env.PUBLIC_URL + '/images/hero-mobile.jpg'}
-        ></img>
+          className={card.logo}
+          src={process.env.PUBLIC_URL + '/images/logo.svg'}
+          alt='Company logo'
+        />
       </header>
-      {/* Hero image */}
-      {/* content */}
+      <picture className={card.hero_image}>
+        <source
+          media='(min-width: 376px)'
+          srcSet={process.env.PUBLIC_URL + '/images/hero-desktop.jpg'}
+        />
+        <img
+          src={process.env.PUBLIC_URL + '/images/hero-mobile.jpg'}
+          alt='a female model, in an orange shirt, posing in front of a plant'
+        />
+      </picture>
+      <div className={card.typography}>
+        <h1 className={card.headline}>
+          <span className={`${card.mainColor} ${card.weightThin}`}>We're</span>{' '}
+          coming soon
+        </h1>
+        <p className={`${card.mainColor} ${card.copy}`}>
+          Hello fellow shoppers! We're currently building our new fashion store.
+          Add your email below to stay up-to-date with announcements and our
+          launch deals.
+        </p>
+        <form>
+          <input placeholder='Email Address'></input>
+          <button>
+            <img
+              src={process.env.PUBLIC_URL + '/images/icon-arrow.svg'}
+              alt='caret pointing right'
+            ></img>
+          </button>
+        </form>
+      </div>
     </main>
   );
 }
